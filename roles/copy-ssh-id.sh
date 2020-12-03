afd29849a297014e87541b7f961de1daebd3d279
John <ansible@prdx-ansible101.ziyotek1.local>
2020-12-03 01:42:00 -0500

    update

--- roles/copy-ssh-id.sh
+++ roles/copy-ssh-id.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for ip in `cat /home/ansible/list_of_servers`; do
+for ip in `cat /home/ansible/roles/list_of_servers`; do
 sshpass -p "password" ssh-copy-id -o StrictHostKeyChecking=no ansible@$ip
 done
